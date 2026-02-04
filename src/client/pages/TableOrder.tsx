b3ee02c40222cb3a744aa4246efa6529a88b874f
amaranceur <amaranceur2@gmail.com>
2026-02-04 15:32:33 +0100

    Describe what you changed

--- src/client/pages/TableOrder.tsx
+++ src/client/pages/TableOrder.tsx
@@ -3,7 +3,7 @@ import { useParams, useSearchParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './TableOrder.css';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://backenddaroui.onrender.com/api';
 
 interface MenuItem {
   id: number;
--- src/offline_client/components/DeliveryDetails/DeliveryDetailsDrawer.tsx
+++ src/offline_client/components/DeliveryDetails/DeliveryDetailsDrawer.tsx
@@ -5,7 +5,7 @@ import { useCart } from "../../context/CartContext";
 import { useTableSession } from "../../context/TableSessionContext";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://backenddaroui.onrender.com';
 
 interface DeliveryDetailsDrawerProps {
   isOpen: boolean;
