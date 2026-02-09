bb6762eb0446b24bdb4ebf1a4bd3fa94bf7b1d18
Anceur <yassineanceur16@gmail.com>
2026-02-09 10:29:04 +0100

    change all type image

--- src/offline_client/components/Sidebar/sidebar.tsx
+++ src/offline_client/components/Sidebar/sidebar.tsx
@@ -7,14 +7,14 @@ import type { Promotion } from "../../lib/api"
 import { useCart } from "../../context/CartContext"
 
 const categories = [
-  { id: "popular", label: "Tous", image: "/images/all.png" },
-  { id: "burger", label: "Burger", image: "/images/Barger.png" },
-  { id: "sandwich", label: "Sandwich", image: "/images/sndwich.png" },
-  { id: "pizza", label: "Pizza", image: "/images/pizza.png" },
-  { id: "plat", label: "Plat", image: "/images/plat.png" },
-  { id: "tacos", label: "Tacos", image: "/images/tacos.png" },
-  { id: "desserts", label: "Desserts", image: "/images/dessert.png" },
-  { id: "drinks", label: "Boissons", image: "/images/drinks.png" },
+  { id: "popular", label: "Tous", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2Fall%20(1).webp?alt=media&token=c4f96695-f9bb-445b-9499-fd2b9033563d" },
+  { id: "burger", label: "Burger", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2FBarger%20(1).webp?alt=media&token=d5c84efd-9c80-44a5-b21b-ea1015f5f377" },
+  { id: "sandwich", label: "Sandwich", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2Fsndwich%20(1).webp?alt=media&token=1cee50e7-107f-489e-99bf-d6d6a1b2b646" },
+  { id: "pizza", label: "Pizza", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2Fpizza%20(1).webp?alt=media&token=0595101d-21a7-4be6-a685-d7a985f39551" },
+  { id: "plat", label: "Plat", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2Fplat%20(1).webp?alt=media&token=8175ab02-3ae2-4396-bfe0-c5b441378db0" },
+  { id: "tacos", label: "Tacos", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2Ftacos%20(1).webp?alt=media&token=adc34345-19da-47a0-b26d-67be79d22620" },
+  { id: "desserts", label: "Desserts", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2Fdessert%20(1).webp?alt=media&token=cc358c46-3707-4ecd-b0d6-d0f0327ce499" },
+  { id: "drinks", label: "Boissons", image: "https://firebasestorage.googleapis.com/v0/b/daroui.firebasestorage.app/o/imageapp%2Fdrinks%20(1).webp?alt=media&token=3c1ba8c4-b5db-40e4-b805-a17f3048b538" },
 ]
 
 type Props = {
